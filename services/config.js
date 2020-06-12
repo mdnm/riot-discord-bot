616c1d3abaa1dc2e173d012782613649c4d3b91a
Felipe S <snowychest@gmail.com>
2020-06-12 06:05:21 -0300

    Some code view, changed require to import as in es6+ and added a config file that will hold all configurable aspects of the application

---
+++ services/config.js
@@ -0,0 +1,10 @@
+export default
+
+//this is a workaround as javascript doesn't support importing pure .json files at the moment
+
+{
+
+    "api_key": "RGAPI-46a16d95-ccce-4ced-985e-77a9215813de",
+    "bot_key": "NzIwNzYwNDczNjAzMDE0Nzk5.XuKq1A.jJRAUKl35lMUu24DTC-aFdGKnkA"
+
+}
