63b77086d62ff7283e2963583fdb5b5db2149725
Mateus de Nardo <mateusdnm@hotmail.com>
2020-06-25 21:40:27 -0300

    Json configuration file

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,7 @@
 import { Client } from '@typeit/discord'
 
+import * as Config from './config/config.json'
+
 export class Main {
   private static _client: Client;
 
@@ -13,7 +15,7 @@ export class Main {
     // In the login method, you must specify the glob string to load your classes (for the framework).
     // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
-      'NzIwNzYwNDczNjAzMDE0Nzk5.XuKq1A.jJRAUKl35lMUu24DTC-aFdGKnkA',
+      Config['discord-token'],
       `${__dirname}/discords/*.ts`, // glob string to load the classes
       `${__dirname}/discords/*.js` // If you compile your bot, the file extension will be .js
     )
